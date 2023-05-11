f2e036fa02a4d0289a640471020a12a4448a9dcf
Abdulkadir Celebi <abdulkadircelebi.ac@gmail.com>
2023-05-11 14:02:39 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+function isPalindrome(str) {
+	const cleanedStr = str.replace(/[\W_]/g, '').toLowerCase();
+	const reversedStr = cleanedStr.split('').reverse().join('');
+	return cleanedStr === reversedStr;
 }
 
 /* 
@@ -12,14 +14,14 @@ function isPalindrome(word) {
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+	// add your own custom tests in here
+	console.log('Expecting: true');
+	console.log('=>', isPalindrome('racecar'));
 
-  console.log("");
+	console.log('');
 
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+	console.log('Expecting: false');
+	console.log('=>', isPalindrome('robot'));
 }
 
 module.exports = isPalindrome;
